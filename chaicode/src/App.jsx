17566797c2c92ac84201b8e91649b928f094683d
Sami1122911 <samikhansk2021@gmail.com>
2025-12-23 23:31:58 +0500

    starting chai our code

---
+++ chaicode/src/App.jsx
@@ -0,0 +1,9 @@
+import Chai from "./Chai";
+function App() {
+  return <>
+  <Chai />
+  <h2>ASDFGHJ</h2>
+  </>;
+}
+
+export default App
---
+++ chaicode/src/Chai.jsx
@@ -0,0 +1,5 @@
+function Chai(){
+    return <h1>Chai fee la BOSDI-K</h1>
+}
+
+export default Chai
